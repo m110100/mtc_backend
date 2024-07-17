2e512c81cf44758be52e0bd6cbf55b56cb6c88fa
denis <deis675skul@gmail.com>
2024-07-17 22:35:45 +0300

    feat: add getUserInfo method for get information about user

--- src/main/java/com/rightcode/mtc/store/repositories/UserRepository.java
+++ src/main/java/com/rightcode/mtc/store/repositories/UserRepository.java
@@ -16,6 +16,8 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
 
+    Optional<User> findByUsername(String username);
+
     @Query("SELECT u FROM User u JOIN u.applications ea WHERE ea.event.id = :eventId AND (:eventStatus IS NULL OR ea.status = :eventStatus) ORDER BY ea.dos ASC")
     Page<User> findUsersByEventIdAndStatus(@Param("eventId") Long eventId, @Param("eventStatus") ApplicationStatus eventStatus, Pageable pageable);
 

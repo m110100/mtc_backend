0fd2c30815f6c2ceac0e0e9e7162495caef29b71
m110100 <shindayoni@gmail.com>
2024-07-18 01:56:36 +0300

    cors registration bean

--- src/main/java/com/rightcode/mtc/configurations/WebServicesConfiguration.java
+++ src/main/java/com/rightcode/mtc/configurations/WebServicesConfiguration.java
@@ -1,11 +1,17 @@
 package com.rightcode.mtc.configurations;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
@@ -15,7 +21,29 @@ import org.springframework.xml.xsd.XsdSchema;
 @EnableWs
 @Configuration
 @EnableTransactionManagement
+@RequiredArgsConstructor
 public class WebServicesConfiguration {
+    private final CorsProps corsProps;
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilterRegistration() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration configuration = new CorsConfiguration();
+
+        configuration.setAllowCredentials(corsProps.isAllowedCredentials());
+        corsProps.getAllowedOrigins().forEach(configuration::addAllowedOrigin);
+        configuration.addAllowedHeader(corsProps.getAllowedHeaders());
+        corsProps.getAllowedMethods().forEach(configuration::addAllowedMethod);
+        configuration.setMaxAge(corsProps.getMaxAge());
+
+        source.registerCorsConfiguration(corsProps.getCorsConfiguration(), configuration);
+
+        FilterRegistrationBean<CorsFilter> bean = new FilterRegistrationBean<>(new CorsFilter(source));
+        bean.setOrder(0);
+
+        return bean;
+    }
+
     @Bean
     public ServletRegistrationBean<MessageDispatcherServlet> messageDispatcherServlet(ApplicationContext context) {
         MessageDispatcherServlet servlet = new MessageDispatcherServlet();

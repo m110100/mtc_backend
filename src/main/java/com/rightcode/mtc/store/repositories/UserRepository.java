cdbfa5d57abcd90f9f7098330a2cf822dfde95d2
denis <deis675skul@gmail.com>
2024-07-17 12:16:32 +0300

    fix getEventParticipant

--- src/main/java/com/rightcode/mtc/store/repositories/UserRepository.java
+++ src/main/java/com/rightcode/mtc/store/repositories/UserRepository.java
@@ -9,8 +9,12 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+
     @Query("SELECT u FROM User u JOIN u.applications ea WHERE ea.event.id = :eventId AND (:eventStatus IS NULL OR ea.status = :eventStatus) ORDER BY ea.dos ASC")
     Page<User> findUsersByEventIdAndStatus(@Param("eventId") Long eventId, @Param("eventStatus") ApplicationStatus eventStatus, Pageable pageable);
 }
